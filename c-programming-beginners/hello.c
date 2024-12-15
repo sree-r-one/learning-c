977749debd1fda96266d2ddc936402c90ffa0d3a
sree <sreeraj_ec@hotmail.com>
2024-12-15 09:55:42 +0800

    Hello world

---
+++ c-programming-beginners/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char characterName[] = "John";
+    int characterAge = 35;
+
+    printf("Hello world! %s %d \n", characterName, characterAge);
+    return 0;
+}
